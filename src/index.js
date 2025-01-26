a5a4b5c7658cd947587a5ddbbad792dad01a9194
Abbas2003 <abbas.mohammad805@gmail.com>
2025-01-26 08:14:42 +0500

    backend working dir updated

--- src/index.js
+++ src/index.js
@@ -3,18 +3,9 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import morgan from "morgan";
-import studentRoute from "./routes/student.routes.js";
 import authRoute from "./routes/auth.routes.js";
 import userRoute from "./routes/user.routes.js";
-import courseRoute from "./routes/course.routes.js";
-import campusRoute from "./routes/campus.routes.js";
-import sectionRoute from "./routes/section.routes.js";
-import batchRoute from "./routes/batch.routes.js";
-import cityRoute from "./routes/city.routes.js";
-import classRoute from "./routes/class.routes.js";
-import quizRoute from "./routes/quiz.routes.js";
-import assignmentRoute from "./routes/assignments.routes.js";
-import userquizRoute from "./routes/userquiz.routes.js";
+
 
 dotenv.config();
 
@@ -25,7 +16,6 @@ const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 app.use(morgan("tiny"));
-console.log("MONGODB_URI", process.env.MONGO_URI);
 
 
 // Auth Routes
@@ -33,9 +23,6 @@ app.use("/api/v1/auth", authRoute);
 
 // Auth Routes
 app.use("/api/v1/user", userRoute);
-
-// City Routes
-app.use("/api/v1/city", cityRoute);
 
 
 
